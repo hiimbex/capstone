78b100d702aded134f952da0b557c2a66253328d
Bex Warner <bexmwarner@gmail.com>
2018-12-02 18:03:23 -0500

    display the big data

--- routes/d3.js
+++ routes/d3.js
@@ -1,30 +1,40 @@
+let data = []
+const mydata = d3.selectAll("#data")
+    .style("color", "blue");
+//console.log('help', mydata[0], typeof mydata[0])
+Object.keys(mydata[0]).forEach(function (key) {
+  data.push(mydata[0][key].innerHTML)
+})
+data.splice(-1,1)
+console.log(data)
+//var data = [10, 5, 12, 15]
+var width = 1000,
+  scaleFactor = 30000,
+  barHeight = 100
 
-var data = [10, 5, 12, 15];
-         
-         var width = 300 
-            scaleFactor = 20, 
-            barHeight = 30;
-         
-         var graph = d3.select("body")
-            .append("svg")
-            .attr("width", width)
-            .attr("height", barHeight * data.length);
-         
-         var bar = graph.selectAll("g")
-            .data(data)
-            .enter()
-            .append("g")
-            .attr("transform", function(d, i) {
-               return "translate(0," + i * barHeight + ")";
-            });
-         bar.append("rect").attr("width", function(d) {
-            return d * scaleFactor;
-         })
-         
-         .attr("height", barHeight - 1);
-         
-         bar.append("text")
-            .attr("x", function(d) { return (d*scaleFactor); })
-            .attr("y", barHeight / 2)
-            .attr("dy", ".35em")
-            .text(function(d) { return d; });
+let height = barHeight * data.length
+
+var graph = d3.select("body")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height)
+
+var bar = graph.selectAll("g")
+  .data(data)
+  .enter()
+  .append("g")
+  .attr("transform", function(d, i) {
+     return "translate(0," + i * barHeight + ")"
+  })
+
+bar.append("rect").attr("width", function(d) {
+  return d.split(': ')[1] * scaleFactor
+})
+
+.attr("height", barHeight - 1)
+
+bar.append("text")
+  .attr("x", function(d) { return (d.split(': ')[1] * scaleFactor) })
+  .attr("y", barHeight / 2)
+  .attr("dy", ".50em")
+  .text(function(d) { return d })
--- routes/index.js
+++ routes/index.js
@@ -10,15 +10,62 @@ router.get('/', function(req, res, next) {
   console.log(process.env.URI)
   MongoClient.connect(uri, function(err, client) {
     const collection = client.db("housing").collection("ahs")
-    collection.find({ AGE1: 21 }).limit(2).toArray(function(err, result) {
+    // 2 for mold means no, 1 means yes
+    //const query = { AGE1: 24 }
+    const query = { AGE1: { $gt: 0 } }
+    const projection = { MOLDKITCH: 1,
+      MOLDBATH: 1,
+      MOLDBEDRM: 1,
+      MOLDBASEM: 1,
+      MOLDLROOM: 1,
+      MOLDOTHER: 1,
+      _id: 0
+    }
+    collection.find(query).project(projection).toArray(function(err, result) {
       if (err) throw err
-      console.log(result)
-      resultz = result
-      res.render('index', { title: resultz[0].age, body: resultz[0]});
+      let counts = { MOLDKITCH: 0,
+        MOLDBATH: 0,
+        MOLDBEDRM: 0,
+        MOLDBASEM: 0,
+        MOLDLROOM: 0,
+        MOLDOTHER: 0
+      }
+      let percents = { MOLDKITCH: 0,
+        MOLDBATH: 0,
+        MOLDBEDRM: 0,
+        MOLDBASEM: 0,
+        MOLDLROOM: 0,
+        MOLDOTHER: 0
+      }
+      let total = result.length
+      result.forEach(function (person) {
+        let molds = Object.keys(person)
+        console.log(person)
+        molds.forEach(function (room) {
+          //console.log(room, person[room])
+          let num = parseInt(person[room].charAt(1), 10)
+          //console.log(num)
+          if (isNaN(num)) {
+            total -= 1
+          } else {
+            if (num === 2) {
+              num = 0
+            }
+            counts[room] += num
+          }
+        })
+      })
+      console.log(counts)
+      Object.keys(counts).forEach(function (item) {
+        // do the math
+        console.log(counts[item], total, counts[item]/total, percents[item])
+        percents[item] = counts[item]/total
+      })
+      //res.render('index', { title: result[0].AGE1, body: result[0]});
+      res.render('index', { title: total, body: percents});
       client.close()
     });
   });
-  //res.render('index', { title: resultz });
 });
 
 module.exports = router;

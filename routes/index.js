b378bd708f28b0b90c80f99a902e3e415f564435
Bex Warner <bexmwarner@gmail.com>
2018-12-06 20:38:58 -0500

    add filtering by citizenship

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,30 @@ const dotenv = require('dotenv')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  let query
+  // Get query parameters
+  let citizen = req.query.citizen
+  console.log('citizen: ', citizen, typeof citizen)
+  if (citizen === '0') { // only show non-citizens
+    query = { AGE1: { $gt: 0 },  CITSHP1: { $in: [ "'5'" ] }}
+  } else if (citizen === '1') { // only show citizens
+    query = { AGE1: { $gt: 0 },  CITSHP1: { $in: [ "'1'", "'2'", "'3'", "'4'" ] }}
+  } else { // show everyone
+    query = { AGE1: { $gt: 0 }}
+  }
+
   let resultz
   var MongoClient = require('mongodb').MongoClient
   const uri = process.env.URI
-  console.log(process.env.URI)
   MongoClient.connect(uri, function(err, client) {
     const collection = client.db("housing").collection("ahs")
+
+    collection.find({ AGE1: 21, CITSHP1: "'1'" }).limit(1).toArray(function(err, result) {
+      console.log(result)
+      resultz = result
+    })
     // 2 for mold means no, 1 means yes
     //const query = { AGE1: 24 }
-    const query = { AGE1: { $gt: 0 } }
     const projection = { MOLDKITCH: 1,
       MOLDBATH: 1,
       MOLDBEDRM: 1,
@@ -21,7 +36,9 @@ router.get('/', function(req, res, next) {
       MOLDOTHER: 1,
       _id: 0
     }
+    console.log(query)
     collection.find(query).project(projection).toArray(function(err, result) {
+      console.log(result)
       if (err) throw err
       let counts = { MOLDKITCH: 0,
         MOLDBATH: 0,
@@ -40,11 +57,8 @@ router.get('/', function(req, res, next) {
       let total = result.length
       result.forEach(function (person) {
         let molds = Object.keys(person)
-        console.log(person)
         molds.forEach(function (room) {
-          //console.log(room, person[room])
           let num = parseInt(person[room].charAt(1), 10)
-          //console.log(num)
           if (isNaN(num)) {
             total -= 1
           } else {
@@ -55,14 +69,12 @@ router.get('/', function(req, res, next) {
           }
         })
       })
-      console.log(counts)
       Object.keys(counts).forEach(function (item) {
         // do the math
-        console.log(counts[item], total, counts[item]/total, percents[item])
         percents[item] = counts[item]/total
       })
       //res.render('index', { title: result[0].AGE1, body: result[0]});
-      res.render('index', { title: total, body: percents});
+      res.render('index', { title: total, body: percents, data: resultz[0]});
       client.close()
     });
   });
